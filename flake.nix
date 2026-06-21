8e9167d3d12880d7c8d6d84bf8ee917dcf40421f
szymon <szymon_jozef@proton.me>
2026-06-21 21:10:02 +0200

    feat: init

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+        build_inputs = with pkgs; [
+          clang
+          gcc
+          cmake
+        ];
+
+        battleships = (
+          with pkgs;
+          stdenv.mkDerivation {
+            pname = "battleships";
+            version = "0.0.1";
+            src = ./.;
+
+            nativeBuildInputs = build_inputs;
+
+            buildPhase = "make -j $NIX_BUILD_CORES";
+            installPhase = ''
+              mkdir -p $out/bin
+            '';
+          }
+        );
+      in
+      rec {
+        defaultApp = flake-utils.lib.mkApp {
+          drv = defaultPackage;
+        };
+
+        defaultPackage = battleships;
+
+        devShell = pkgs.mkShell {
+
+          packages = build_inputs;
+        };
+      }
+    );
+}
