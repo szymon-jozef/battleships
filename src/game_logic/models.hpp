52503cc32931c3a5a9c1d3e09fd4ef7a6740b1ae
szymon <szymon_jozef@proton.me>
2026-06-22 13:41:54 +0200

    feat: implement ship class

---
+++ src/game_logic/models.hpp
@@ -0,0 +1,80 @@
+#pragma once
+
+#include <boost/uuid/uuid.hpp>
+#include <string>
+#include <vector>
+
+/* --- Ships --- */
+
+/// ShipType defines how many field the ship takes
+enum class ShipType {
+  FourMaster = 4,
+  ThreeMaster = 3,
+  TwoMaster = 2,
+  OneMaster = 1
+};
+
+/// Every ship has it's own type, UUID and hp.
+/// HP is the same as ship length
+class Ship {
+  boost::uuids::uuid id;
+  ShipType type;
+  unsigned int health;
+
+public:
+  Ship(ShipType type);
+  bool isSunk() const;
+  /// Decrement ship health by one
+  void hit();
+};
+
+/* --- Fields --- */
+enum class FieldState { EMPTY, TAKEN, MISSED, HIT, SUNK };
+
+/// Every field stores it's  current state and a pointer to the ship
+/// that's there, if any. Otherwise nullptr
+class Field {
+  Field();
+  FieldState state;
+  // One ship may be on multiple fields so we use an shared_ptr
+  std::shared_ptr<Ship> ship;
+};
+
+enum class ShotResult {
+  MISS,
+  HIT,
+  SUNK,
+  ALREADY_SHOT,
+  OUT_OF_BOUNDS,
+};
+
+/* --- Grids --- */
+class BaseGrid {
+  // we use short int, because no one wants to play with 23894723984723489
+  // fields
+  const unsigned short int WIDTH;
+  const unsigned short int HEIGHT;
+  // should reserve width * height in the constructor
+  std::vector<std::vector<Field>> grid;
+
+public:
+  BaseGrid(const unsigned short int width, const unsigned short int height);
+  FieldState getFieldState(unsigned short int row,
+                           unsigned short int column) const;
+  virtual std::string asString() const;
+};
+
+class Board : public BaseGrid {
+public:
+  Board(const unsigned short int width, const unsigned short int height);
+  bool placeShip(ShipType shipType, unsigned short int startRow,
+                 unsigned short int startColumn, bool isHorizontal);
+  ShotResult takeShot(unsigned short int row, unsigned short int column);
+  virtual std::string asString() const override;
+};
+
+class Radar : public BaseGrid {
+public:
+  Radar(const unsigned short int width, const unsigned short int height);
+  void markShotResult(unsigned short int row, unsigned short int);
+};
---
+++ src/game_logic/ship.cpp
@@ -0,0 +1,12 @@
+#include "models.hpp"
+#include <boost/uuid.hpp>
+
+Ship::Ship(ShipType type)
+    : type(type), health(static_cast<unsigned int>(type)) {
+  boost::uuids::random_generator gen;
+  id = gen();
+}
+
+bool Ship::isSunk() const { return health <= 0; }
+
+void Ship::hit() { health--; }
