beedbfa07b6390034ee388507249c7817f75ed57
szymon <szymon_jozef@proton.me>
2026-07-01 22:44:43 +0200

    feat: add client connecting

---
+++ src/networking/client.cpp
@@ -0,0 +1,32 @@
+#include "client.hpp"
+#include <boost/asio/ip/basic_resolver.hpp>
+#include <exception>
+#include <memory>
+#include <spdlog/spdlog.h>
+
+namespace battleship {
+namespace networking {
+Client::~Client() {
+  disconnect();
+}
+
+bool Client::connect(const std::string &host, const uint16_t port) {
+  try {
+    boost::asio::ip::tcp::resolver resolver(context);
+    boost::asio::ip::tcp::resolver::results_type endpoints = resolver.resolve(host, std::to_string(port));
+
+    connection =
+        std::make_shared<Connection>(Connection::Owner::CLIENT, context, boost::asio::ip::tcp::socket(context), queIn);
+
+    connection->connectToServer(endpoints);
+
+    thread = std::thread([this]() { context.run(); });
+  } catch (std::exception &e) {
+    spdlog::error("[Client] failed trying to connect: {}", e.what());
+    return false;
+  }
+  return true;
+}
+
+} // namespace networking
+} // namespace battleship
---
+++ src/networking/client.hpp
@@ -0,0 +1,27 @@
+#include "connection.hpp"
+#include "data_types.hpp"
+#include "messages.hpp"
+#include <boost/asio/io_context.hpp>
+#include <cstdint>
+#include <string>
+#include <thread>
+
+namespace battleship {
+namespace networking {
+class Client {
+  boost::asio::io_context context;
+  std::thread thread;
+  std::shared_ptr<Connection> connection;
+  MessageQueue queIn;
+
+public:
+  Client() = default;
+  ~Client();
+
+  bool connect(const std::string &host, const uint16_t port);
+  void disconnect();
+  bool isConnected() const;
+  void send(const Message &msg);
+};
+} // namespace networking
+} // namespace battleship
