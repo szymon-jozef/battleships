e59e798924dbdd13de852c11d5acf11765afa856
szymon <szymon_jozef@proton.me>
2026-07-05 11:21:00 +0200

    refactor: split game-manager into cpp and hpp files

--- src/game_manager/game_manager.cpp
+++ src/game_manager/game_manager.cpp
@@ -1,94 +1,86 @@
+#include "game_manager.hpp"
 #include <../game_logic/logic_models.hpp>
-#include <../networking/client.hpp>
 #include <cstdint>
 
 namespace battleship {
 namespace gameManager {
-class GameManager {
-  battleship::networking::Client client;
-  battleship::logic::Player player;
 
-  const std::string serverUrl;
-  const uint16_t serverPort;
+// TODO! Server should verify that both players boards are of the same size.
+GameManager::GameManager(std::string playerName, const std::string &serverUrl, const uint16_t serverPort)
+    : player(playerName)
+    , serverUrl(serverUrl)
+    , serverPort(serverPort) {
+  client.setRecievingAttackFunc(
+      [this](unsigned short int row, unsigned short int column) { return this->player.recieveShot(row, column); });
 
-public:
-  // TODO! Server should verify that both players boards are of the same size.
-  GameManager(std::string playerName, const std::string &serverUrl, const uint16_t serverPort = 6767)
-      : player(playerName)
-      , serverUrl(serverUrl)
-      , serverPort(serverPort) {
-    client.setRecievingAttackFunc(
-        [this](unsigned short int row, unsigned short int column) { return this->player.recieveShot(row, column); });
+  client.setMarkResultFunc([this](logic::FieldState state, unsigned short int row, unsigned short int column) {
+    return this->player.markShotResult(state, row, column);
+  });
 
-    client.setMarkResultFunc([this](logic::FieldState state, unsigned short int row, unsigned short int column) {
-      return this->player.markShotResult(state, row, column);
-    });
-
-    client.setOnShotRecieve([this]() {
-      if (player.isDead()) {
-        client.sendLost();
-        return true;
-      }
-      return false;
-    });
-  }
-
-  void connect() {
-    client.connect(player.getName(), serverUrl, serverPort);
-  }
-
-  void updateClient() {
-    client.update();
-  }
-
-  bool placeShip(unsigned short int row, unsigned short int column, bool isHorizontal) {
-    bool result = player.placeShip(row, column, isHorizontal);
-    if (!result) {
-      return false;
+  client.setOnShotRecieve([this]() {
+    if (player.isDead()) {
+      client.sendLost();
+      return true;
     }
+    return false;
+  });
+}
 
-    if (!player.hasShips()) {
-      client.sendGameStatus(networking::GameStatus::WAR);
-    }
+void GameManager::connect() {
+  client.connect(player.getName(), serverUrl, serverPort);
+}
 
-    return true;
+void GameManager::updateClient() {
+  client.update();
+}
+
+bool GameManager::placeShip(unsigned short int row, unsigned short int column, bool isHorizontal) {
+  bool result = player.placeShip(row, column, isHorizontal);
+  if (!result) {
+    return false;
   }
 
-  // TODO! Change client so it puts FieldStates on some kind of queue, so we can play sounds, etc... (GUI_EVENTS)
-
-  void makeShot(unsigned short int row, unsigned short int column) {
-    // TODO! Server should send to the clients which turn it is and client shouldn't be able to shoot when it's not its
-    // turn
-    client.sendAttack(row, column);
+  if (!player.hasShips()) {
+    client.sendGameStatus(networking::GameStatus::WAR);
   }
 
-  networking::GameStatus getCurrentGameStatus() const {
-    return client.currentGameStatus;
-  }
+  return true;
+}
 
-  logic::FieldState getBoardField(unsigned short int row, unsigned short int column) {
-    return player.getBoardState(row, column);
-  }
+// TODO! Change client so it puts FieldStates on some kind of queue, so we can play sounds, etc... (GUI_EVENTS)
 
-  logic::FieldState getRadarField(unsigned short int row, unsigned short int column) {
-    return player.getRadarState(row, column);
-  }
+void GameManager::makeShot(unsigned short int row, unsigned short int column) {
+  // TODO! Server should send to the clients which turn it is and client shouldn't be able to shoot when it's not its
+  // turn
+  client.sendAttack(row, column);
+}
 
-  std::string getPlayerName() const {
-    return client.name;
-  }
+networking::GameStatus GameManager::getCurrentGameStatus() const {
+  return client.currentGameStatus;
+}
 
-  std::string getEnemyName() const {
-    return client.enemyName;
-  }
+logic::FieldState GameManager::getBoardField(unsigned short int row, unsigned short int column) {
+  return player.getBoardState(row, column);
+}
 
-  std::string getLoserName() const {
-    return client.loserName;
-  }
+logic::FieldState GameManager::getRadarField(unsigned short int row, unsigned short int column) {
+  return player.getRadarState(row, column);
+}
 
-  bool isGameWon() {
-    return client.enemyName == client.loserName;
-  }
-};
+std::string GameManager::getPlayerName() const {
+  return client.name;
+}
+
+std::string GameManager::getEnemyName() const {
+  return client.enemyName;
+}
+
+std::string GameManager::getLoserName() const {
+  return client.loserName;
+}
+
+bool GameManager::isGameWon() {
+  return client.enemyName == client.loserName;
+}
 } // namespace gameManager
 } // namespace battleship
---
+++ src/game_manager/game_manager.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include <../game_logic/logic_models.hpp>
+#include <../networking/client.hpp>
+#include <cstdint>
+
+namespace battleship {
+namespace gameManager {
+
+/// @brief Class that's the main abstraction layer over network client and game logic.
+class GameManager {
+  battleship::networking::Client client;
+  battleship::logic::Player player;
+
+  const std::string serverUrl;
+  const uint16_t serverPort;
+
+public:
+  GameManager(std::string playerName, const std::string &serverUrl, const uint16_t serverPort = 6767);
+
+  void connect();
+  void updateClient();
+  bool placeShip(unsigned short int row, unsigned short int column, bool isHorizontal);
+  void makeShot(unsigned short int row, unsigned short int column);
+
+  networking::GameStatus getCurrentGameStatus() const;
+  logic::FieldState getBoardField(unsigned short int row, unsigned short int column);
+  logic::FieldState getRadarField(unsigned short int row, unsigned short int column);
+
+  std::string getPlayerName() const;
+  std::string getEnemyName() const;
+  std::string getLoserName() const;
+
+  bool isGameWon();
+};
+} // namespace gameManager
+} // namespace battleship
