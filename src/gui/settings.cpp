1bdff2c9f1aac31154678f3551e587998790e48f
szymon <szymon_jozef@proton.me>
2026-07-10 13:39:25 +0200

    refactor: split settings into header and source

---
+++ src/gui/settings.cpp
@@ -0,0 +1,30 @@
+#include "settings.hpp"
+
+namespace battleship {
+namespace gui {
+
+Settings::Settings(GameContext &gameContext, Texture2D &background)
+    : Scene(gameContext, background)
+    , widgets(gameContext, 0.1, 0.1, 0.4f, 0.1) {
+  backgroundTint = LIGHTGRAY;
+
+  widgets.push_back_label("Player options", BLACK);
+  widgets.push_back_textInput_with_label("Player name: ", gameContext.playerName, BLACK, gameContext.playerName);
+  widgets.push_back_button("Go back", [&gameContext]() { gameContext.guiState = GuiState::MAIN_MENU; });
+}
+
+void Settings::update() {
+  Scene::update();
+  if (GetKeyPressed() == KEY_ESCAPE) {
+    gameContext.guiState = GuiState::MAIN_MENU;
+  }
+  widgets.update_all();
+}
+
+void Settings::draw() {
+  Scene::draw();
+  widgets.draw_all();
+}
+
+} // namespace gui
+} // namespace battleship
--- src/gui/settings.hpp
+++ src/gui/settings.hpp
@@ -10,30 +10,11 @@ class Settings : public Scene {
   WidgetsVector widgets;
 
 public:
-  Settings(GameContext &gameContext, Texture2D &background)
-      : Scene(gameContext, background)
-      , widgets(gameContext, 0.1, 0.1, 0.4f, 0.1) {
-    backgroundTint = LIGHTGRAY;
+  Settings(GameContext &gameContext, Texture2D &background);
+  ~Settings() = default;
 
-    widgets.push_back_label("Player options", BLACK);
-    widgets.push_back_textInput_with_label("Player name: ", gameContext.playerName, BLACK, gameContext.playerName);
-    widgets.push_back_button("Go back", [&gameContext]() { gameContext.guiState = GuiState::MAIN_MENU; });
-  }
-
-  ~Settings() {}
-
-  void update() override {
-    Scene::update();
-    if (GetKeyPressed() == KEY_ESCAPE) {
-      gameContext.guiState = GuiState::MAIN_MENU;
-    }
-    widgets.update_all();
-  }
-
-  void draw() override {
-    Scene::draw();
-    widgets.draw_all();
-  }
+  void update() override;
+  void draw() override;
 };
 } // namespace gui
 } // namespace battleship
