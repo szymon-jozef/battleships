3addc732327ac861c0cd19e07de54e81a03c8dc4
szymon <szymon_jozef@proton.me>
2026-08-05 18:09:44 +0200

    fix: dealocating after windowclose()

--- src/gui/gui.cpp
+++ src/gui/gui.cpp
@@ -17,55 +17,57 @@ int run() {
   SetWindowMinSize(GetScreenWidth() * 0.3f, GetScreenHeight() * 0.3f);
   // SetTargetFPS(60);
 
-  GameContext gameContext = GameContext();
+  {
+    GameContext gameContext = GameContext();
 
-  std::atomic<GuiState> &currentState = gameContext.guiState;
-  GuiState previousState = currentState;
-  bool shouldClose = false;
+    std::atomic<GuiState> &currentState = gameContext.guiState;
+    GuiState previousState = currentState;
+    bool shouldClose = false;
 
-  std::unique_ptr<Scene> currentScene;
-  currentScene = std::make_unique<MainMenu>(gameContext, gameContext.assetsManager.bg1);
+    std::unique_ptr<Scene> currentScene;
+    currentScene = std::make_unique<MainMenu>(gameContext, gameContext.assetsManager.bg1);
 
-  while (!WindowShouldClose() && !shouldClose) {
-    // === Switching or dominating ===
-    if (currentState != previousState) {
-      previousState = currentState;
+    while (!WindowShouldClose() && !shouldClose) {
+      // === Switching or dominating ===
+      if (currentState != previousState) {
+        previousState = currentState;
 
-      switch (currentState) {
-      case GuiState::MAIN_MENU:
-        currentScene = std::make_unique<MainMenu>(gameContext, gameContext.assetsManager.bg1);
-        break;
-      case GuiState::MODE_SELECTION:
-        currentScene = std::make_unique<ModeSelection>(gameContext, gameContext.assetsManager.bg2);
-        break;
-      case GuiState::GAME:
-        currentScene = std::make_unique<Game>(gameContext, gameContext.assetsManager.playBackground);
-        break;
-      case GuiState::JOIN_SERVER:
-        currentScene = std::make_unique<JoinServer>(gameContext, gameContext.assetsManager.bg2);
-        break;
-      case GuiState::GAME_FINISH:
-        currentScene = std::make_unique<GameFinish>(gameContext, gameContext.assetsManager.bg3);
-        break;
-      case GuiState::SETTINGS:
-        currentScene = std::make_unique<Settings>(gameContext, gameContext.assetsManager.bg1);
-        break;
-      case GuiState::QUIT:
-        shouldClose = true;
-        break;
+        switch (currentState) {
+        case GuiState::MAIN_MENU:
+          currentScene = std::make_unique<MainMenu>(gameContext, gameContext.assetsManager.bg1);
+          break;
+        case GuiState::MODE_SELECTION:
+          currentScene = std::make_unique<ModeSelection>(gameContext, gameContext.assetsManager.bg2);
+          break;
+        case GuiState::GAME:
+          currentScene = std::make_unique<Game>(gameContext, gameContext.assetsManager.playBackground);
+          break;
+        case GuiState::JOIN_SERVER:
+          currentScene = std::make_unique<JoinServer>(gameContext, gameContext.assetsManager.bg2);
+          break;
+        case GuiState::GAME_FINISH:
+          currentScene = std::make_unique<GameFinish>(gameContext, gameContext.assetsManager.bg3);
+          break;
+        case GuiState::SETTINGS:
+          currentScene = std::make_unique<Settings>(gameContext, gameContext.assetsManager.bg1);
+          break;
+        case GuiState::QUIT:
+          shouldClose = true;
+          break;
+        }
       }
+
+      // === Updating ===
+      currentScene->update();
+
+      // === Drawing ===
+      BeginDrawing();
+      ClearBackground(BLACK);
+
+      currentScene->draw();
+
+      EndDrawing();
     }
-
-    // === Updating ===
-    currentScene->update();
-
-    // === Drawing ===
-    BeginDrawing();
-    ClearBackground(BLACK);
-
-    currentScene->draw();
-
-    EndDrawing();
   }
 
   CloseWindow();
